044601ac5981f560b1a7b9d30e2471b2bc0ee7ca
Klaven Jones <klavensjones@gmail.com>
2021-11-24 15:21:17 -0500

    Completed Brands Page

--- components/views/BrandsPage/Brands.js
+++ components/views/BrandsPage/Brands.js
@@ -1,13 +1,12 @@
 import { BrandsList } from "./Brands.style";
 
-
 const brands = [
   {
     image:
       "https://images.unsplash.com/photo-1611930022073-b7a4ba5fcccd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     name: "Brand Name",
-    snippet: "Long headline to turn",
-    caption:
+    caption: "Long headline to turn",
+    snippet:
       "Long headline to turn your visitors into users Long headline to turn your visitors into users",
     date: "10 jun 2020 19:40"
   },
@@ -15,8 +14,8 @@ const brands = [
     image:
       "https://images.unsplash.com/photo-1567721913486-6585f069b332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
     name: "Brand Name",
-    snippet: "Long headline to turn",
-    caption:
+    caption: "Long headline to turn",
+    snippet:
       "Long headline to turn your visitors into users Long headline to turn your visitors into users Long headline to turn your visitors into users",
     date: "10 jun 2020 19:40"
   },
@@ -24,8 +23,8 @@ const brands = [
     image:
       "https://images.unsplash.com/photo-1511556820780-d912e42b4980?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     name: "Brand Name",
-    snippet: "Long headline to turn",
-    caption:
+    caption: "Long headline to turn",
+    snippet:
       "Long headline to turn your visitors into users Long headline to turn your visitors into users Long headline to turn your visitors into users",
     date: "10 jun 2020 19:40"
   },
@@ -33,8 +32,8 @@ const brands = [
     image:
       "https://images.unsplash.com/photo-1511556820780-d912e42b4980?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     name: "Brand Name",
-    snippet: "Long headline to turn",
-    caption:
+    caption: "Long headline to turn",
+    snippet:
       "Long headline to turn your visitors into users Long headline to turn your visitors into users Long headline to turn your visitors into users",
     date: "10 jun 2020 19:40"
   }
--- components/views/BrandsPage/Brands.style.js
+++ components/views/BrandsPage/Brands.style.js
@@ -4,6 +4,14 @@ const reversedOrder = css`
   ${tw`order-last lg:order-first`}
 `;
 
+const standardOrder = css`
+  ${tw`order-first lg:order-last`}
+`;
+
+const captionRight = css`
+  ${tw`lg:ml-auto`}
+`;
+
 export const BrandListContainer = ({ children }) => (
   <section class="py-20">
     <div class="container px-4 mx-auto">{children}</div>
@@ -11,7 +19,7 @@ export const BrandListContainer = ({ children }) => (
 );
 
 export const BrandListHeadline = () => (
-  <div tw="max-w-3xl mb-16">
+  <div tw="max-w-3xl mb-16 px-4">
     <span tw="inline-block mb-2 text-sm uppercase text-red-500">
       Lorem ipsum
     </span>
@@ -24,9 +32,9 @@ export const BrandListHeadline = () => (
 );
 
 export const BrandListItem = ({ brand, reversed }) => (
-  <div tw="grid grid-cols-1 md:grid-cols-2">
-    <div tw="w-full px-4">
-      <div tw="text-left lg:max-w-xs">
+  <div tw="grid grid-cols-1 md:grid-cols-5 mb-16">
+    <div tw="w-full col-span-1 md:col-span-2 px-4">
+      <div css={[tw`text-left lg:max-w-xs`, reversed && captionRight]}>
         <span tw="text-xs px-2 py-1 rounded uppercase text-red-500 font-semibold bg-red-50 text-red-500">
           {brand.name}
         </span>
@@ -37,13 +45,14 @@ export const BrandListItem = ({ brand, reversed }) => (
         <p tw="text-lg text-gray-600 leading-loose">{brand.snippet}</p>
       </div>
     </div>
+
     <div
       css={[
-        `w-full px-4 mb-8 lg:mb-0`,
-        `${reversed ? reversedOrder : `order-first lg:order-last`}`
+        tw`w-full col-span-1 md:col-span-3 px-4 mb-8 lg:mb-0`,
+        reversed ? reversedOrder : standardOrder
       ]}
     >
-      <div tw="h-96">
+      <div tw="h-96 w-full">
         <img
           class="w-full h-full object-cover rounded-lg"
           src={brand.image}
@@ -54,11 +63,27 @@ export const BrandListItem = ({ brand, reversed }) => (
   </div>
 );
 
+export const ShowMoreBrands = () => (
+  <div tw="text-center">
+    <button
+      tw="px-6 py-3 text-gray-50 font-semibold bg-red-500 hover:bg-gray-600 rounded leading-loose"
+      href="#"
+    >
+      View More Brands
+    </button>
+  </div>
+);
+
 export const BrandsList = ({ brands }) => (
   <BrandListContainer>
     <BrandListHeadline />
     {brands.map((brand, index) => (
-      <BrandListItem brand={brand} reversed={(index + 1) % 2 === 0} />
+      <BrandListItem
+        key={brand.name}
+        brand={brand}
+        reversed={(index + 1) % 2 === 0}
+      />
     ))}
+    <ShowMoreBrands />
   </BrandListContainer>
 );
